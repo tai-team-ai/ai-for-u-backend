d712be48bfcdc7431ed8a10e3ba314714b832b36
Jacob Petterle <jacobpetterle+aiforu@gmail.com>
2023-03-11 15:10:44 +0000

    add examples

--- src/api/lambda/ai_tools_api/routers/sandbox_chatgpt.py
+++ src/api/lambda/ai_tools_api/routers/sandbox_chatgpt.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import sys
 from typing import Optional
-from fastapi import APIRouter, Response, status, Request
+from fastapi import APIRouter, status
 
 from pydantic import constr
 
@@ -20,8 +20,24 @@ class SandBoxChatGPTResponse(CamelCaseModel):
     gpt_response: str
 
 
+class SandBoxChatGPTExamples(CamelCaseModel):
+    example_names: list[str]
+    examples: list[str]
+
+
+@router.get("/sandbox-chatgpt-examples", response_model=SandBoxChatGPTExamples, status_code=status.HTTP_200_OK)
+async def sandbox_chatgpt_examples() -> SandBoxChatGPTExamples:
+    """
+    Get examples for sandbox-chatgpt.
+
+    Returns:
+        examples: Examples for sandbox-chatgpt.
+    """
+    return SandBoxChatGPTExamples(example_names=[], examples=[])
+
+
 @router.post("/sandbox-chatgpt", response_model=SandBoxChatGPTResponse, status_code=status.HTTP_200_OK)
-async def catchy_title_creator(sandbox_chatgpt_request: SandBoxChatGPTRequest) -> SandBoxChatGPTResponse:
+async def sandbox_chatgpt(sandbox_chatgpt_request: SandBoxChatGPTRequest) -> SandBoxChatGPTResponse:
     """
     Get response from openAI Turbo GPT-3 model.
 
