20435130d66b558eef32160fa418ed4af58e1ab5
Jacob Petterle <jacobpetterle+aiforu@gmail.com>
2023-03-31 06:22:30 +0000

    add truncator function

--- src/api/lambda/ai_tools_api/gpt_turbo.py
+++ src/api/lambda/ai_tools_api/gpt_turbo.py
@@ -2,8 +2,11 @@ from __future__ import annotations
 from pydantic import BaseModel
 from enum import Enum
 import openai
-from utils import does_user_have_enough_tokens_to_make_request
+from loguru import logger
+from utils import does_user_have_enough_tokens_to_make_request, docstring_parameter
 
+
+MODEL_CONTEXT_WINDOW = 4096
 
 class Role(Enum):
     USER = "user"
@@ -51,7 +54,52 @@ class GPTTurboChatSession(BaseModel):
         """Add a message to the chat session and return a new chat session model"""
         new_messages = self.messages + (message,)
         return GPTTurboChatSession(messages=new_messages)
+
+def can_user_make_request(user_uuid: str, expected_token_count: int) -> None:
+    """
+    Check if a user has enough tokens to make a request.
+
+    Args:
+        user_uuid: The user's UUID.
+        expected_token_count: The expected token count of the request.
+
+    Returns:
+        can_user_make_request: Whether the user can make the request.
+        tokens_allowed: The number of tokens the user is allowed to use.
+    """
+    can_make_request, tokens_allowed = does_user_have_enough_tokens_to_make_request(
+        user_uuid=user_uuid,
+        expected_token_count=expected_token_count,
+    )
+    if not can_make_request:
+        raise Exception("User does not have enough tokens to make request. Token quota: {tokens_allowed}, Tokens required for request: {expected_token_count}")
     
+
+@docstring_parameter(MODEL_CONTEXT_WINDOW)
+def truncate_chat_session(chat_session: GPTTurboChatSession, max_tokens_for_response: int) -> GPTTurboChatSession:
+    """
+    Truncate the chat session to the model context window ({0})
+
+    This function truncates the chat session to the model context window. This is 
+    necessary because the model can only handle a certain number of tokens in the 
+    context window. This truncation is done by removing the oldest messages until
+    the tokens from the session and the tokens from the request are less than the
+    model context window.
+
+    Args:
+        chat_session: The chat session to truncate.
+    
+    Returns:
+        chat_session: The truncated chat session.
+    """
+    chat_history_cumulative_token_count = 0
+    for chat in chat_session.messages:
+        chat_history_cumulative_token_count += chat.token_count
+    while chat_history_cumulative_token_count + max_tokens_for_response > MODEL_CONTEXT_WINDOW:
+        chat_history_cumulative_token_count -= chat_session.messages[0].token_count
+        chat_session = GPTTurboChatSession(messages=chat_session.messages[1:])
+        logger.info(chat_session)
+    return chat_session
 
 
 def get_gpt_turbo_response(
@@ -82,16 +130,12 @@ def get_gpt_turbo_response(
         {"role": "system", "content": system_prompt}
     ]
     chat_history_cumulative_token_count = 0
+    chat_session = truncate_chat_session(chat_session, max_tokens)
     for chat in chat_session.messages:
         chat_history_cumulative_token_count += chat.token_count
         prompt_messages.append(chat.dict(exclude={"token_count"}))
     tokens_for_request = chat_history_cumulative_token_count + max_tokens
-    can_user_make_request, tokens_allowed = does_user_have_enough_tokens_to_make_request(
-        user_uuid=uuid,
-        expected_token_count=tokens_for_request,
-    )
-    if not can_user_make_request:
-        raise Exception(f"User does not have enough tokens to make request. Token quota: {tokens_allowed}, Tokens required for request: {tokens_for_request}")
+    can_user_make_request(uuid, tokens_for_request)
 
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
