78538c029954ca34fb781e7a858f1a719d1f2c6b
Jacob Petterle <jacobpetterle+aiforu@gmail.com>
2023-04-17 23:48:31 +0000

    update cover letter prompt

--- src/api/lambda/ai_tools_api/routers/cover_letter_writer.py
+++ src/api/lambda/ai_tools_api/routers/cover_letter_writer.py
@@ -104,11 +104,16 @@ AI_PURPOSE = " ".join(ENDPOINT_NAME.split("-")).lower()
 class CoverLetterWriterInsructions(BaseAIInstructionModel):
     """You are an expert {0}. I will provide a resume, job posting, and company name (if given) to write a cover letter for.
 
-    I will also provide a list of skills to highlight from the resume. You should respond with a cover letter that is tailored to the job posting and company, highlights the user's skills, and demonstrates enthusiasm for the company and role.
+    I will also provide a list of skills to highlight from the resume. You should respond with a cover letter that is tailored to the job posting and company, highlights the my skills, and demonstrates enthusiasm for the company and role.
 
-    **Instructions that I may provide you:**
+    If you do not feel like that the job isn't a good fit for me, you should do your best to highlight the skills that you think are most relevant to the job posting in addition to the ones I have asked to highlight.
+
+    **Instructions that I may provide you to assist you as you write my cover letter:**
+    * job_posting: The job posting to generate a cover letter for.
+    * company_name: The name of the company you are applying to.
+    * skills_to_highlight_from_resume: The skills to highlight from the resume. If this is not provided, you should highlight the skills that you think are most relevant to the job posting or most impressive if the skills do not directly align with the job description..
     * tone: The tone that you should use when writing the cover letter. Here are the possible tones: {1}.
-    * skills_to_highlight_from_resume: The skills to highlight from the resume. If this is not provided, you should highlight the skills that you think are most relevant to the job posting.
+    * resume: The resume to use when writing the cover letter. This is guaranteed to be provided.
     """
     job_posting: constr(min_length=1, max_length=10000) = Field(
         ...,
@@ -150,7 +155,7 @@ class CoverLetterWriterRequest(CoverLetterWriterInsructions):
     resume: constr(min_length=1, max_length=10000) = Field(
         ...,
         title="Resume",
-        description="Your resume. Don't worry too much about formatting.",
+        description="Your resume. Don't worry too much if the formatting isn't perfect.",
     )
 
 
