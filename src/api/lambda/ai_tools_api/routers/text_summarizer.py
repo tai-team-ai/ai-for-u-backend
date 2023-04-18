0ed152b4321f17d02e12ba9331b210eccbdb3f48
Jacob Petterle <jacobpetterle+aiforu@gmail.com>
2023-04-18 03:16:18 +0000

    field limits

--- src/api/lambda/ai_tools_api/routers/text_summarizer.py
+++ src/api/lambda/ai_tools_api/routers/text_summarizer.py
@@ -6,7 +6,11 @@ import threading
 from typing import Optional
 from fastapi import APIRouter, Response, status, Request
 import openai
+<<<<<<< Updated upstream
 from pydantic import constr
+=======
+from pydantic import conint, Field
+>>>>>>> Stashed changes
 
 sys.path.append(Path(__file__, "../").absolute())
 from gpt_turbo import GPTTurboChatSession, GPTTurboChat, Role, get_gpt_turbo_response
@@ -56,10 +60,33 @@ class TextSummarizerRequest(BaseAIInstructionModel):
     Inherit from BaseAIInstructionModel:    
     """
     __doc__ += BaseAIInstructionModel.__doc__
+<<<<<<< Updated upstream
     text_to_summarize: str
     include_summary_sentence: Optional[bool] = True
     number_of_bullets: Optional[int] = None
     number_of_action_items: Optional[int] = None
+=======
+    text_to_summarize: str = Field(
+        ...,
+        title="Text to Summarize",
+        description="The text that you wanted summarized. (e.g. articles, notes, trnascripts, etc.)",
+    )
+    include_summary_sentence: Optional[bool] = Field(
+        default=True,
+        title="Include Summary Sentence",
+        description="Whether or not to include a summary sentence in the response.",
+    )
+    number_of_bullets: Optional[conint(ge=0, le=8)] = Field(
+        default=3,
+        title="Number of Bullets",
+        description="The number of bullet points to include in the response.",
+    )
+    number_of_action_items: Optional[conint(ge=0, le=8)] = Field(
+        default=None,
+        title="Number of Action Items",
+        description="The number of action items to include in the response. Action items are often applicable for meeting notes, lectures, etc.",
+    )
+>>>>>>> Stashed changes
 
 
 
