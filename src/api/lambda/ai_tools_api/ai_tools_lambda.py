1bb59300a7237e80adfb540d786d0f4a4aea19e8
Jacob Petterle <jacobpetterle+aiforu@gmail.com>
2023-04-26 14:29:54 +0000

    fix dynamo logic in lambda function to properly initialize the table

--- src/api/lambda/ai_tools_api/ai_tools_lambda.py
+++ src/api/lambda/ai_tools_api/ai_tools_lambda.py
@@ -40,6 +40,7 @@ from utils import (
     USER_TOKEN_HEADER_NAME,
     is_user_authenticated,
     EXAMPLES_ENDPOINT_POSTFIX,
+    get_user_uuid_from_jwt_token,
 )
 
 logger = logging.getLogger()
@@ -108,10 +109,9 @@ def initialize_user_db(uuid: UUID, is_user_authenticated: bool):
     try:
         user_data_model: UserDataTableModel = UserDataTableModel.get(str(uuid))
     except (Model.DoesNotExist):
-        user_data_model = UserDataTableModel(str(uuid))
-    user_data_model.update(actions=[
-        UserDataTableModel.authenticated_user.set(is_user_authenticated),
-    ])
+        logger.info("User not found in DB. Is user authenticated? %s", is_user_authenticated)
+        user_data_model = UserDataTableModel(str(uuid), authenticated_user=is_user_authenticated)
+        user_data_model.save()
 
 def create_fastapi_app():
     """Create FastAPI app."""
@@ -152,11 +152,14 @@ def create_fastapi_app():
                 raise UserTokenNotFoundError("User UUID not found.") from e
         if uuid:
             user_token = request.headers.get(USER_TOKEN_HEADER_NAME, None)
+
             authenticated = False
-            if user_token is not None:
-                authenticated = is_user_authenticated(uuid, user_token)
+            if user_token:
+                jwt_uuid = get_user_uuid_from_jwt_token(user_token)
+                authenticated = is_user_authenticated(uuid, UUID(jwt_uuid))
             os.environ[AUTHENTICATED_USER_ENV_VAR_NAME] = str(authenticated)
-            initialize_user_db(uuid, authenticated)
+            uuid_to_use = jwt_uuid if authenticated else uuid
+            initialize_user_db(uuid_to_use, authenticated)
             logger.info(f"Authenticated: {authenticated}")
         response = await call_next(request)
         prepare_response(response, request)
